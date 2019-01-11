62e5bc800a550fc84de830c38b138f666a2c62ca
Pieter <pdutoit@fullfacing.com>
2019-01-11 16:08:25 +0200

    set up page routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,60 @@
-import React from 'react'
-import { BrowserRouter as Router } from "react-router-dom"
+import React, { Component } from 'react'
+import { Switch, Route, Redirect, withRouter } from "react-router-dom"
 
 import './App.css'
 
-const App = () => (
-  <Router>
-    <div>
-      Virtue Group
-    </div>
-  </Router>
-)
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
 
-export default App
+const navItems = [
+  {
+    id: 0,
+    name: 'Home',
+    path: '/home'},
+  {
+    id: 1,
+    name: 'About',
+    path: '/about'
+  },
+  {
+    id: 2,
+    name: 'Contact Us',
+    path: '/contact-us'
+  }
+]
+
+const matchNavItem = path => {
+  return navItems.find(item => item.path === path)
+}
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    const activeNavItem = matchNavItem(props.location.pathname)
+
+    this.state = {
+      active: activeNavItem || navItems[0]
+    }
+  }
+
+
+  render() {
+    const {active} = this.state
+
+    return <div>
+      {`you're on ${active.path}`}
+      <Switch>
+        <Route path='/home' component={Home}/>
+        <Route path='/about' component={About}/>
+        <Route path='/contact-us' component={Contact}/>
+        <Redirect to='/home'/>
+      </Switch>
+    </div>
+  }
+}
+
+
+export default withRouter(App)
