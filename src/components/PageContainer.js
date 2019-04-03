4ecebb62bb16f2cf5e27f1b51596e0fd5b1aee9f
Pieter <pieter_du_toit@hotmail.com>
2019-04-03 19:10:29 +0200

    routing bug fixes

--- src/components/PageContainer.js
+++ src/components/PageContainer.js
@@ -17,6 +17,7 @@ const pages = {
 const PageContainer = ({match}) => {
   const { page } = match.params
   const pageFound = Object.keys(pages).includes(page)
+  
   return pageFound ? pages[page] : <Redirect to="/home"/>
 }
 
