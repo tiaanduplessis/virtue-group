0d9225c94372a79dbf5dcbb560c6365db5527ecc
Pieter <pdutoit@fullfacing.com>
2019-06-12 13:49:05 +0200

    add image on-load

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
-import React, { Fragment } from 'react'
+import React, { Component, Fragment } from 'react'
+
+import BackgroundImageOnLoad from 'background-image-on-load';
 
 import homeStyles from './styles'
 import { withStyles } from '@material-ui/core/styles'
@@ -8,35 +10,53 @@ import homeImageLowQuality from '../images/home-image-low-quality.png'
 
 import { Typography } from '@material-ui/core'
 
-const Home = ({classes}) => {
-  return (
-    <Fragment>
-      <div className="homeImage"/>
+class Home extends Component {
 
-      <Typography
-        align="center"
-        variant="h4"
-        classes={{
-          h4: classes.subtitle
-        }}
-      >
-        Services that we provide
-      </Typography>
+  state = {
+    bgIsLoaded: false
+  }
 
-      <style jsx>{`
-        .homeImage {
-          width: 100vw;
-          padding-top: calc(50vh - 70px);
-          overflow: hidden;
-          background-image: url(${homeImageLowQuality});
-          background-image: url(${homeImage});
-          background-repeat: no-repeat;
-          background-size: cover;
-          background-color: rgba(23, 38, 47, 0.1);
-        }
-      `}</style>
-    </Fragment>
-  )
+  render() {
+    const { bgIsLoaded } = this.state
+    const { classes } = this.props
+
+    return (
+      <Fragment>
+        <div className="homeImage">
+          <BackgroundImageOnLoad
+            src={homeImage}
+            onLoadBg={() =>
+              this.setState({
+              bgIsLoaded: true
+            })}
+            onError={err => console.log('error', err)}
+          />
+        </div>
+
+        <Typography
+          align="center"
+          variant="h4"
+          classes={{
+            h4: classes.subtitle
+          }}
+        >
+          Services that we provide
+        </Typography>
+
+        <style jsx>{`
+          .homeImage {
+            width: 100vw;
+            padding-top: calc(50vh - 70px);
+            overflow: hidden;
+            background-image: url(${bgIsLoaded ? homeImage: homeImageLowQuality});
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-color: rgba(23, 38, 47, 0.1);
+          }
+        `}</style>
+      </Fragment>
+    )
+  }
 }
 
 export default withStyles(homeStyles)(Home)
